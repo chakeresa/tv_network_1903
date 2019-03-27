db0221fd11ab068046ba2415ca4e0a4c73aef423
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-27 09:25:24 -0600

    Network init and add_show methods and corresponding tests

--- test/network_test.rb
+++ test/network_test.rb
@@ -3,4 +3,31 @@ require 'minitest/pride'
 require './lib/network'
 
 class NetworkTest < Minitest::Test
+
+  def setup
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+
+    @nbc = Network.new("NBC")
+  end
+
+  def test_it_exists
+    assert_instance_of Network, @nbc
+  end
+
+  def test_it_inits_with_a_name_and_an_empty_ary_of_shows
+    assert_equal "NBC", @nbc.name
+    assert_equal [], @nbc.shows
+  end
+
+  def test_it_can_add_a_show
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
 end
